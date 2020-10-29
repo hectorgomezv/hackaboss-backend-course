b118fba490a65889e18ff6e88594b93696681147
Hector Gomez <hector.gomez.varela@accenture.com>
2020-10-29 21:01:17 +0100

    Initial commit

---
+++ ejercicio-1/index.js
@@ -0,0 +1,5 @@
+// Enunciado:
+// Usando el módulo fs de NodeJS, muestra por pantalla el contenido de file.txt
+// Puedes usar callbacks o promises, aunque lo recomendable es lo segundo.
+
+// PISTA: el contenido se lee como un Buffer de datos, usa UTF-8 para leerlo.
