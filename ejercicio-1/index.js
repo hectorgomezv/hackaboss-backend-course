e9e171868afea89a29f53029cdded4ba7f528ebb
Hector Gomez <hector.gomez.varela@accenture.com>
2020-10-30 16:07:52 +0100

    Resolución ejercicio 1

--- ejercicio-1/index.js
+++ ejercicio-1/index.js
@@ -3,3 +3,16 @@
 // Puedes usar callbacks o promises, aunque lo recomendable es lo segundo.
 
 // PISTA: el contenido se lee como un Buffer de datos, usa UTF-8 para leerlo.
+
+const fs = require('fs').promises;
+
+async function leer() {
+  try {
+    const contenido = await fs.readFile('file.txt', 'utf-8');
+    console.log(contenido);
+  } catch (err) {
+    console.error('Ha ocurrido un error');
+  }
+}
+
+leer();
