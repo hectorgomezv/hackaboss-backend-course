8793d7fb57579494cda1ef1f78298bb5ea062270
Hector Gomez <hector.gomez.varela@accenture.com>
2020-10-30 18:09:27 +0100

    Solucion ejercicio 2

--- core-modules/fs-module/2-ejercicio-writeFile/index.js
+++ core-modules/fs-module/2-ejercicio-writeFile/index.js
@@ -3,3 +3,19 @@
 // Después llama a esta función para escribir un fichero file.txt en el directorio actual el contenido 'Hola Backend!'
 
 // Puedes usar callbacks o promises, aunque lo recomendable es lo segundo.
+
+const fs = require('fs').promises;
+const path = require('path');
+
+async function escribeFichero(ruta, data) {
+  try {
+    await fs.writeFile(ruta, data);
+  } catch (err) {
+    console.error('Ha ocurrido un error');
+  }
+}
+
+const ruta = path.join(__dirname, 'file.txt');
+const data = 'Hola Backend!';
+
+escribeFichero(ruta, data);
