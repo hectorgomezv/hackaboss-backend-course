cc82af4d9e8d89893204cfdf6acdece335072011
Hector Gomez <hector.gomez.varela@accenture.com>
2020-10-30 19:18:18 +0100

    Resolucion ejercicio 4

--- core-modules/fs-module/4-ejercicio-stat/index.js
+++ core-modules/fs-module/4-ejercicio-stat/index.js
@@ -5,3 +5,33 @@
 // La función deberá imprimir el string 'No existe' si el Path que le pasamos no existe.
 
 // Puedes usar callbacks o promises, aunque lo recomendable es lo segundo.
+
+const fs = require('fs').promises;
+const path = require('path');
+
+async function imprimeEstado(ruta) {
+  try {
+    const info = await fs.stat(ruta);
+
+    const esDirectorio = info.isDirectory();
+    
+    if (esDirectorio) {
+      console.log('Es un directorio');
+    }
+
+    if (info.isFile()) {
+      console.log('Es un archivo');
+    }
+
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log('No existe el archivo o directorio');
+    } else {
+      throw err;
+    }
+  }
+}
+
+const ruta = path.join(__dirname, 'djghkjfgd');
+
+imprimeEstado(ruta);
