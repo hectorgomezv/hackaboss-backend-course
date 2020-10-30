9ac30d998b7cd302f1c0a27b2754748610aa2ab1
Hector Gomez <hector.gomez.varela@accenture.com>
2020-10-30 16:46:02 +0100

    Add exercise 5

---
+++ core-modules/fs-module/3-ejercicio-mkdir/index.js
@@ -0,0 +1,6 @@
+// Enunciado:
+// Usando el módulo fs de NodeJS, escribe una función que acepte como argumento un nombre de directorio (String).
+// La función deberá crear ese directorio dentro del directorio actual.
+// Si se produce algun error, deberá imprimir el error.
+
+// Puedes usar callbacks o promises, aunque lo recomendable es lo segundo.
---
+++ core-modules/fs-module/5-ejercicio-delete/index.js
@@ -0,0 +1,7 @@
+// Enunciado:
+// Usando el módulo fs de NodeJS, escribe una función que acepte como argumento un nombre de archivo.
+// La función deberá borrar ese archivo del directorio actual.
+// La función deberá imprimir el string 'No existe' si el nombre de archivo que le pasamos no existe.
+// Si se produce algun error, deberá imprimir el error.
+
+// Puedes usar callbacks o promises, aunque lo recomendable es lo segundo.
