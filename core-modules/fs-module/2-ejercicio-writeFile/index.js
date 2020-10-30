21e067f04023c4646cbab0be1164544a600bf52b
Hector Gomez <hector.gomez.varela@accenture.com>
2020-10-30 16:34:41 +0100

    Add exercise 3

---
+++ core-modules/fs-module/2-ejercicio-writeFile/index.js
@@ -0,0 +1,4 @@
+// Enunciado:
+// Usando el módulo fs de NodeJS, escribe una función que acepte como argumentos un Path y un string.
+// Después llama a esta función para escribir un fichero file.txt en el directorio actual el contenido 'Hola Backend!'
+// Puedes usar callbacks o promises, aunque lo recomendable es lo segundo.
---
+++ core-modules/fs-module/3-ejercicio-stat/index.js
@@ -0,0 +1,6 @@
+// Enunciado:
+// Usando el módulo fs de NodeJS, escribe una función que acepte como argumentos un Path.
+// La función deberá imprimir el string 'Es un directorio' si el Path que le pasamos es un directorio.
+// La función deberá imprimir el string 'Es un archivo' si el Path que le pasamos es un archivo.
+// La función deberá imprimir el string 'No existe' si el Path que le pasamos no existe.
+// Puedes usar callbacks o promises, aunque lo recomendable es lo segundo.
